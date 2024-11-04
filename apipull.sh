7bb5047f5cec10f32c56e01bd8e03b344c0a2df3
Tibor Rogulja <tibor.rogulja@productive.io>
2024-11-04 12:34:11 +0100

    new data:migrate toolkit

--- apipull.sh
+++ apipull.sh
@@ -22,62 +22,15 @@ if echo "$pull_output" | grep -q -E "(Gemfile|Gemfile.lock)"; then
   bundle install
 fi
 
-# Step 3: bundle exec rails db:migrate
-echo '🤖 Running database migrations...'
+# Step 3: run schema migrations
+echo '🤖 Running schema migrations...'
 migrate_command="bundle exec rails db:migrate"
 eval "${migrate_command}"
 
-# Step 4: offer data migrations (if any new)
-migration_dir="lib/tasks/migrations"
-migration_state_file="$HOME/.productive_api_migration_state"
-if [[ -f "$migration_state_file" ]]; then
-  IFS=$'\n'
-  migration_state=($(cat "$migration_state_file"))
-  unset IFS
-else
-  touch "$migration_state_file"
-  migration_state=()
-fi
-
-for migration_file in "$migration_dir"/**/*.rake; do
-  file_name=$(basename "$migration_file")
-  file_path=$(dirname "$migration_file" | sed "s|$migration_dir/||")
-  migration_file_name="$file_path/$file_name"
-  migration_tasks=$(grep -E '^ *task [^:]+:' "$migration_file" | sed -E 's/^ *task ([^:]+):.*/\1/')
-
-  for migration_task in $migration_tasks; do
-    migration_identifier="$migration_file_name - $migration_task"
-
-    if [[ ! " ${migration_state[*]} " =~ " ${migration_identifier} " ]]; then
-      echo
-      echo "🤖 New migration detected: $migration_identifier"
-      read -p "🤖 Run it, never run it or just skip it for now? [y/n/*] " -n 1 -r
-      echo
-      if [[ $REPLY = "y" ]]; then
-        migration_command="bundle exec rails migrations:$migration_task"
-        echo "🤖 Running migration..."
-        if eval "${migration_command}"; then
-          echo "$migration_identifier" >> "$migration_state_file"
-          echo "🤖 Migration successful: $migration_task"
-        else
-          echo "🤖 Migration failed: $migration_task"
-          read -p "🤖 Ignore it? [y/n] " -n 1 -r
-          echo
-          if [[ $REPLY = "y" ]]; then
-            echo "🤖 Ignoring migration..."
-            echo "$migration_identifier" >> "$migration_state_file"
-          fi
-        fi
-      elif [[ $REPLY = "n" ]]; then
-        echo "🤖 Ignoring migration..."
-        echo "$migration_identifier" >> "$migration_state_file"
-      else
-        echo "🤖 Skipping migration for now..."
-      fi
-    fi
-  done
-
-done
+# Step 4: run data migrations
+echo '🤖 Running data migrations...'
+migrate_command="bundle exec rails data:migrate"
+eval "${migrate_command}"
 
 # Step 5: git restore .
 echo '🤖 Cleaning up...'
